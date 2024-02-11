9cfd57e2e1b57ee4025285ea25f4fb77ac090edd
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2024-02-11 16:22:46 +0300

    Initial commit

---
+++ src/main/java/com/kadiraksoy/HibernateAnnotationExample/HibernateAnnotationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.HibernateAnnotationExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAnnotationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAnnotationExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kadiraksoy/HibernateAnnotationExample/HibernateAnnotationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.HibernateAnnotationExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateAnnotationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
